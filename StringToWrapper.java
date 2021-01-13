2e8642e5cebb601b2811a8bfeb1e0ac92dac6d23
Nargiza Konushalieva <64344753+Lebedok@users.noreply.github.com>
2021-01-12 21:52:38 -0600

    Add files via upload

---
+++ StringToWrapper.java
@@ -0,0 +1,13 @@
+package WrapperClass;
+
+public class StringToWrapper {
+
+    public static void main(String[] args) {
+
+        String num = "12.5";
+        Double db = Double.valueOf(num);
+        System.out.println(db);
+
+
+    }
+}
